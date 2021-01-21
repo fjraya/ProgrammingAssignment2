ba1d3c1483ddb1d572965c24adb4785f95e3ff62
Francisco Javier Raya Márquez <francisco.raya@gmail.com>
2021-01-21 08:46:01 +0100

    identation

--- cachematrix.R
+++ cachematrix.R
@@ -4,31 +4,31 @@
 ## Enhance matrix datatype with some methods that allow to store result of solve operation and retrieve it later.
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) i <<- solve
-  getsolve <- function() i
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) i <<- solve
+    getsolve <- function() i
+    list(set = set, get = get,
+        setsolve = setsolve,
+        getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 ## determine if solve result is previously calculated and stored and return it, and calculate and store this value otherwise.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  i <- x$getsolve()
-  if(!is.null(i)) {
-    message("getting cached data")
-    return(i)
-  }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setsolve(i)
-  i
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getsolve()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setsolve(i)
+    i
 }
